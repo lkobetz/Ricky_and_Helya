6f9b795451e0b3e2cbdbf489ae953b8b8940dd98
lkobetz <larissa.kobetz@gmail.com>
2020-06-26 11:55:03 -0400

    fixed schedule styling with css grid

--- src/sections/schedule/Schedule.js
+++ src/sections/schedule/Schedule.js
@@ -10,11 +10,15 @@ export default class Schedule extends React.Component {
       items: [
         {
           time: "12:00-1:00",
+        },
+        {
           title: "Ceremony",
           description: `Our ceremony will include both modern and traditional Persian elements. The following will provide some background information on the Persian traditions we've decided to include.`,
         },
         {
           time: "1:00-2:00",
+        },
+        {
           title: "Photos / Refreshments",
           description: `While the wedding party sticks around the garden for photos, all
           other guests may help themselves to refreshments in the
@@ -23,6 +27,8 @@ export default class Schedule extends React.Component {
         },
         {
           time: "2:00-12:00",
+        },
+        {
           title: "Reception",
           description: `There will be champagne, speeches, dinner and dancing! Bring your dancing shoes! Or take them off and go wild!`,
         },
@@ -39,7 +45,12 @@ export default class Schedule extends React.Component {
         </div>
         <div id="schedule_container">
           {this.state.items.map((item) => {
-            return <ScheduleItem item={item} key={item.title} />;
+            return (
+              <ScheduleItem
+                item={item}
+                key={item.title ? item.title : item.time}
+              />
+            );
           })}
         </div>
       </div>
--- src/sections/schedule/ScheduleItem.js
+++ src/sections/schedule/ScheduleItem.js
@@ -1,14 +1,22 @@
 import React from "react";
 
 export default function ScheduleItem(props) {
+  let name = Object.keys(props.item);
+  name = name.join("_");
   return (
-    <div className="schedule_item">
-      <p className="time">{props.item.time}</p>
-      {/* <p className="dot">●</p> */}
-      <div className="description_container">
-        <p className="description_title">{props.item.title}</p>
-        <p className="description_text">{props.item.description}</p>
-      </div>
+    <div className={name}>
+      {props.item.time ? (
+        <div className="schedule_time">
+          <p className="time">{props.item.time}</p>
+        </div>
+      ) : (
+        <div className="schedule_event">
+          <div className="description_container">
+            <p className="description_title">{props.item.title}</p>
+            <p className="description_text">{props.item.description}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
