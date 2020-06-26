fcdec56bee43486e0df5bbd82385ab2b6e7e0c45
lkobetz <larissa.kobetz@gmail.com>
2020-06-26 13:10:57 -0400

    added tourism categories

---
+++ src/sections/tourism/Category.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Category(props) {
+  return <p>{props.item.name}</p>;
+}
--- src/sections/tourism/Tourism.js
+++ src/sections/tourism/Tourism.js
@@ -1,13 +1,26 @@
 import React from "react";
 import flower from "../images/yello flower.png";
+import Category from "./Category.js";
+import "./Tourism.scss";
 
 export default function Schedule() {
+  const items = [
+    { name: "Downtown" },
+    { name: "Nature" },
+    { name: "Restaurants" },
+    { name: "Bars" },
+  ];
   return (
     <div id="tourism" className="section">
       <div className="section_title">
         <img className="leftSmallFlower" src={flower} alt="" />
         <p>Tourism</p>
         <img className="rightSmallFlower" src={flower} alt="" />
+      </div>
+      <div id="tourism_content">
+        {items.map((item) => {
+          return <Category item={item} />;
+        })}
       </div>
     </div>
   );
