b64702353e0607cdb5f7061ad326223567d8e150
lkobetz <larissa.kobetz@gmail.com>
2020-06-29 14:06:42 -0400

    styled map

--- src/sections/venue/Map.js
+++ src/sections/venue/Map.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-
+import "./Map.scss";
 import mapApiKey from "./secrets";
 import React, { Component } from "react";
 import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
@@ -9,14 +9,14 @@ class Map extends Component {
       withGoogleMap((props) => (
         <GoogleMap
           defaultCenter={{ lat: 39.2554715, lng: -77.3977137 }}
-          defaultZoom={13}
+          defaultZoom={15}
         ></GoogleMap>
       ))
     );
     return (
-      <div>
+      <div id="map_container">
         <GoogleMapComponent
-          containerElement={<div style={{ height: `500px`, width: "500px" }} />}
+          containerElement={<div id="map" />}
           mapElement={<div style={{ height: `100%` }} />}
           loadingElement={<div style={{ height: `100%` }} />}
           googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${mapApiKey}`}
