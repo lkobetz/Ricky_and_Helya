5f0e4088f73b261e691e5217793406e54ab7cbbd
lkobetz <larissa.kobetz@gmail.com>
2020-06-30 17:11:11 -0400

    created app and navbar folders

--- src/app/App.js
+++ src/app/App.js
@@ -3,21 +3,21 @@ import "./App.scss";
 import bouquet1 from "./images/bouquet-9a.png";
 import bouquet2 from "./images/bouquet-10.png";
 import header_photo from "./images/header_photo.png";
-import NavBar from "./Navbar";
-import RSVP from "./sections/rsvp/RSVP";
-import Schedule from "./sections/schedule/Schedule";
-import Venue from "./sections/venue/Venue";
-import Registry from "./sections/registry/Registry";
-import Transportation from "./sections/transportation/Transportation";
-import Photos from "./sections/photos/Photos";
-import Tourism from "./sections/tourism/Tourism";
-import WeddingParty from "./sections/weddingParty/WeddingParty";
+import NavBar from "../navbar/Navbar";
+import RSVP from "../sections/rsvp/RSVP";
+import Schedule from "../sections/schedule/Schedule";
+import Venue from "../sections/venue/Venue";
+import Registry from "../sections/registry/Registry";
+import Transportation from "../sections/transportation/Transportation";
+import Photos from "../sections/photos/Photos";
+import Tourism from "../sections/tourism/Tourism";
+import WeddingParty from "../sections/weddingParty/WeddingParty";
 import HRgif from "./images/HR_gif.gif";
 import HRbeach from "./images/HR_kiss_beach.jpg";
 import HRsparkler from "./images/HR_sparkler.jpg";
 import CouplePhoto from "./CouplePhoto";
 import firebase from "firebase";
-import { firebaseConfig } from "./dbconfig";
+import { firebaseConfig } from "../dbconfig";
 
 firebase.initializeApp(firebaseConfig);
 
@@ -54,8 +54,8 @@ function App() {
       <Photos />
       <div id="footer">
         <p>
-          This site was designed and built by{" "}
-          <a href="https://lkobetz.github.io/">Larissa Kobetz.</a>
+          This website was designed and built by{" "}
+          <a href="https://lkobetz.github.io/">Larissa Kobetz</a>.
         </p>
       </div>
     </div>
