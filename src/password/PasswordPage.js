482e4591a45a324b60e4f77c5488ef83391f7451
lkobetz <larissa.kobetz@gmail.com>
2020-07-02 16:28:33 -0400

    added error message to password page

--- src/password/PasswordPage.js
+++ src/password/PasswordPage.js
@@ -8,9 +8,18 @@ import bouquet2 from "../images/bouquet-10.png";
 export default function PasswordPage() {
   const [password, changePassword] = useState("");
   const [correct, changeCorrect] = useState(false);
+  const [text1, changeText1] = useState(
+    `Welcome to Helya and Ricky's wedding website!`
+  );
+  const [text2, changeText2] = useState(
+    `Please enter the password from your invitation to enter:`
+  );
   function checkPassword() {
     if (password === secretPassword) {
       changeCorrect(true);
+    } else {
+      changeText1(`Sorry, that password is incorrect!`);
+      changeText2(`Please try again:`);
     }
   }
   return correct ? (
@@ -20,12 +29,8 @@ export default function PasswordPage() {
       <div id="pw-flowers">
         <img id="leftBouquet" src={bouquet1} alt="" />
         <div id="pw-box">
-          <p className="info-text">
-            Welcome to Helya and Ricky's wedding website!
-          </p>{" "}
-          <p className="info-text">
-            Please enter the password from your invitation to enter:
-          </p>
+          <p className="info-text">{text1}</p>{" "}
+          <p className="info-text">{text2}</p>
           <input
             className={"pw-input"}
             type="password"
