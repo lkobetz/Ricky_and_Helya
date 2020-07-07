d3d617c7b02e3435e9c7cd036d65b070b212ecab
lkobetz <larissa.kobetz@gmail.com>
2020-07-07 18:30:30 -0400

    added links to website and map from tourism list

--- src/sections/tourism/LocationItem.js
+++ src/sections/tourism/LocationItem.js
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function LocationItem(props) {
-  return <p className="list-text">➺ {props.info.text}</p>;
+  const site = props.info.url ? true : false;
+  const map = props.info.map ? true : false;
+  return (
+    <div className="list-item">
+      <p className="list-text">➺ {props.info.text}</p>
+      {site && (
+        <a href={props.info.url}>
+          <p className="list-link">Site</p>
+        </a>
+      )}
+      {map && (
+        <a href={props.info.map}>
+          <p className="list-link">Map</p>
+        </a>
+      )}
+    </div>
+  );
 }
