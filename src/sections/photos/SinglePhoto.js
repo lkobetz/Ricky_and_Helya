9c300a92cdfcc4ed2c1562e560254f0e6510906e
lkobetz <larissa.kobetz@gmail.com>
2020-07-09 12:43:29 -0400

    on photo hover changed to on photo click

--- src/sections/photos/SinglePhoto.js
+++ src/sections/photos/SinglePhoto.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SinglePhoto(props) {
+  const [className, changeClass] = useState("single-photo-container");
+  function handleClick() {
+    if (className === "single-photo-container") {
+      changeClass("active-photo");
+    } else {
+      changeClass("single-photo-container");
+    }
+  }
   return (
-    <div className="single-photo-container">
+    <div className={className} onClick={handleClick}>
       <img src={props.photo.src} alt="" />
     </div>
   );
